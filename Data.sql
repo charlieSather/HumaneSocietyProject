b15306a645dbacb727416211593bf2379f9e2813
Charlie Sather <charliesather18@gmail.com>
2020-02-10 09:56:08 -0600

    Finished sql statements that populate database

--- Data.sql
+++ Data.sql
@@ -1,86 +1,54 @@
-  
+INSERT INTO Categories VALUES('dog');
+INSERT INTO Categories VALUES('cat');
+INSERT INTO Categories VALUES('monkey');
+INSERT INTO Categories VALUES('fish');
+INSERT INTO Categories VALUES('rodent');
+
+INSERT INTO DietPlans VALUES('dogplan','purina',3);
+INSERT INTO DietPlans VALUES('catplan','friskies',2);
+INSERT INTO DietPlans VALUES('monkeyplan','bananas',3);
+INSERT INTO DietPlans VALUES('fishplan','fishflakes',1);
+INSERT INTO DietPlans VALUES('rodentplan','foodpellets',1);
 
 
-
-INSERT INTO Categories VALUES('');
-INSERT INTO Categories VALUES('');
-INSERT INTO Categories VALUES('');
-INSERT INTO Categories VALUES('');
-INSERT INTO Categories VALUES('');
-
-
-INSERT INTO DietPlans VALUES('','',);
-INSERT INTO DietPlans VALUES('','',);
-INSERT INTO DietPlans VALUES('','',);
-INSERT INTO DietPlans VALUES('','',);
-INSERT INTO DietPlans VALUES('','',);
-
-/*
-CREATE TABLE Animals (AnimalId INTEGER IDENTITY (1,1) PRIMARY KEY, 
-Name VARCHAR(50), Weight INTEGER, Age INTEGER
-, Demeanor VARCHAR(50), 
-KidFriendly BIT, PetFriendly BIT, 
-Gender VARCHAR(50), AdoptionStatus VARCHAR(50),
-CategoryId INTEGER FOREIGN KEY REFERENCES Categories(CategoryId), 
-DietPlanId INTEGER FOREIGN KEY REFERENCES DietPlans(DietPlanId), 
-EmployeeId INTEGER FOREIGN KEY REFERENCES Employees(EmployeeId));
-*/
+INSERT INTO Animals VALUES('Frisky',10,6,'Lazy, Lethargic',1,1,'Female','Available',2,2,2);
+INSERT INTO Animals VALUES('Nemo',2,4,'Curious',1,1,'Male','Available',4,4,1);
+INSERT INTO Animals VALUES('Wrex',200,7,'Aggressive',1,0,'Male','Pending',1,1,4);
+INSERT INTO Animals VALUES('Rafiki',100,75,'Wise',1,1,'Male','Available',3,3,3);
+INSERT INTO Animals VALUES('Stuart Little',3,1,'Curious',1,1,'Male','Available',5,5,5);
 
 
-INSERT INTO Animals VALUES('','','','','','','','','','','');
-INSERT INTO Animals VALUES('','','','','','','','','','','');
-INSERT INTO Animals VALUES('','','','','','','','','','','');
-INSERT INTO Animals VALUES('','','','','','','','','','','');
-INSERT INTO Animals VALUES('','','','','','','','','','','');
+INSERT INTO Rooms VALUES (1,1);
+INSERT INTO Rooms VALUES (2,2);
+INSERT INTO Rooms VALUES (3,3);
+INSERT INTO Rooms VALUES (4,4);
+INSERT INTO Rooms VALUES (5,5);
+INSERT INTO Rooms VALUES (6,null);
+INSERT INTO Rooms VALUES (7,null);
+INSERT INTO Rooms VALUES (8,null);
+INSERT INTO Rooms VALUES (9,null);
+INSERT INTO Rooms VALUES (10,null);
 
 
-
-INSERT INTO Rooms VALUES ('','');
-INSERT INTO Rooms VALUES ('','');
-INSERT INTO Rooms VALUES ('','');
-INSERT INTO Rooms VALUES ('','');
-INSERT INTO Rooms VALUES ('','');
-INSERT INTO Rooms VALUES ('','');
-INSERT INTO Rooms VALUES ('','');
-INSERT INTO Rooms VALUES ('','');
-INSERT INTO Rooms VALUES ('','');
-INSERT INTO Rooms VALUES ('','');
-INSERT INTO Rooms VALUES ('','');
-
-/*
-CREATE TABLE Clients (ClientId INTEGER IDENTITY (1,1) PRIMARY KEY, 
-FirstName VARCHAR(50), LastName VARCHAR(50), UserName VARCHAR(50), Password VARCHAR(50), 
-AddressId INTEGER FOREIGN KEY REFERENCES Addresses(AddressId), Email VARCHAR(50));
-*/
-
-INSERT INTO Clients VALUES('','','','','','');
-INSERT INTO Clients VALUES('','','','','','');
-INSERT INTO Clients VALUES('','','','','','');
-INSERT INTO Clients VALUES('','','','','','');
-INSERT INTO Clients VALUES('','','','','','');
-
-/*
-CREATE TABLE Addresses (AddressId INTEGER IDENTITY (1,1) PRIMARY KEY, 
-AddressLine1 VARCHAR(50), City VARCHAR(50), USStateId INTEGER FOREIGN KEY REFERENCES USStates(USStateId),  Zipcode INTEGER);
-*/ 
-
-INSERT INTO Addresses VALUES('','','','');
-INSERT INTO Addresses VALUES('','','','');
-INSERT INTO Addresses VALUES('','','','');
-INSERT INTO Addresses VALUES('','','','');
-INSERT INTO Addresses VALUES('','','','');
+INSERT INTO Clients VALUES('Stan','Johnson','stjohn','iliketurtles',1,'stjohn@gmail.com');
+INSERT INTO Clients VALUES('Robert','Williams','bobwill','iamthebob',2,'iamthebob@gmail.com');
+INSERT INTO Clients VALUES('Darren','Sharper','formadden','hardesthittin',3,'formadden@gmail.com');
+INSERT INTO Clients VALUES('George','Washington','notthepresident','ichopwood',4,'ichopwood@gmail.com');
+INSERT INTO Clients VALUES('Kanye','West','closedonsundays','chickfila',5,'closedonsundays@gmail.com');
 
 
-/*
-CREATE TABLE Employees (EmployeeId INTEGER IDENTITY (1,1) PRIMARY KEY, 
-FirstName VARCHAR(50), LastName VARCHAR(50), UserName VARCHAR(50), Password VARCHAR(50), EmployeeNumber INTEGER, Email VARCHAR(50));
-*/
+INSERT INTO Addresses VALUES('313 N Plankinton Ave Suite #209','Milwaukee','49','53203');
+INSERT INTO Addresses VALUES('175 Coffee St.','Brooklyn','32','11223');
+INSERT INTO Addresses VALUES('2910 Hartway Street','Yankton','41','57078');
+INSERT INTO Addresses VALUES('7281 Big Rock Cove Ave.','Merton','49','53056');
+INSERT INTO Addresses VALUES('7946 Country Ave.','San Jose','5','95112');
 
-INSERT INTO Employees VALUES('','','','','','');
-INSERT INTO Employees VALUES('','','','','','');
-INSERT INTO Employees VALUES('','','','','','');
-INSERT INTO Employees VALUES('','','','','','');
-INSERT INTO Employees VALUES('','','','','','');
+
+INSERT INTO Employees VALUES('Zac','Melton','zamelto','ilikecats',1,'zamelto@humane.org');
+INSERT INTO Employees VALUES('Charlie','Sather','crsather','ilikedogs',2,'crsather@humane.org');
+INSERT INTO Employees VALUES('Billy','Mays','bmays','oxyclean',3,'bmays@humane.org');
+INSERT INTO Employees VALUES('Dwayne','Johnson','therock','ilikerocks',4,'therock@humane.org');
+INSERT INTO Employees VALUES('Giannis','Antetokounmpo','greekfreak','iamtall',5,'greekfreak@humane.org');
 
 
 
